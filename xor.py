b0c4a88521a4bbe3638474f3b015253621b621d6
Alec Morgan <alecnmorgan@gmail.com>
2019-07-18 13:38:17 -0700

    Add XOR MLP demo

---
+++ xor.py
@@ -0,0 +1,36 @@
+"""
+XOR is the canonical example of a function that 
+cannot be learned by a linear model because
+it is not linearly separable.
+"""
+import numpy as np 
+from train import train
+from neural_net import NeuralNet
+from layers import Linear, Tanh
+
+inputs = np.array([
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1]
+])
+
+targets = np.array([
+    [1, 0], 
+    [0, 1],
+    [0, 1],
+    [1, 0]
+])
+
+
+net = NeuralNet([
+    Linear(input_size=2, output_size=2),
+    Tanh(),
+    Linear(input_size=2, output_size=2)
+])
+
+train(net, inputs, targets)
+
+for x, y in zip(inputs, targets):
+    predicted = net.forward(x)
+    print(x, predicted, y)
