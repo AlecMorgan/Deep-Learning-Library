12b2a5ea1921144021a9bf6d82c857305cbca6f2
Alec Morgan <alecnmorgan@gmail.com>
2019-07-19 17:05:48 -0700

    Relocate XOR demo to Jupyter notebook

---
+++ dl_lib/eval.py
@@ -0,0 +1,51 @@
+"""
+A small testing suite with support for multi-label prediction.
+
+TODO(Alec): Complete and validate all functions. 
+"""
+import numpy as np
+from dl_lib.tensor import Tensor
+from dl_lib.neural_net import NeuralNet
+
+
+class IllegalArgumentError(ValueError):
+    pass
+
+# Accuracy: (tp + tn) / (tp + tn + fp + fn)
+# Precision: (tp) / (tp + fp)
+# Recall: (tp) / (tp + fn)
+
+def score_accuracy(targets: Tensor,
+                   predictions: Tensor, 
+                   mode: str = "classification",
+                   thres: float = 0.5) -> float:
+    """
+    Evaluate the neural network's accuracy performance.
+    """
+    if mode == "classification":
+        predictions = map(lambda n: 1 if n > thres else 0, predictions)
+        predictions = np.array(list(predictions))
+        score = (predictions == targets).sum()
+    elif mode == "regression":
+        raise NotImplementedError
+    else: 
+        raise IllegalArgumentError
+    return score
+    
+def score_precision(targets: Tensor,
+                    predictions: Tensor, 
+                    mode: str = "classification",
+                    thres: float = 0.5) -> float:
+    """
+    Evaluate thee neural network's precision performance.
+    """
+    raise NotImplementedError
+
+def score_recall(targets: Tensor,
+                 predictions: Tensor, 
+                 mode: str = "classification",
+                 thres: float = 0.5) -> float:
+    """
+    Evaluate the neural network's recall performance.
+    """
+    raise NotImplementedError
